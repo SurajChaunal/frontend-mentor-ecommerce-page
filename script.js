0c645359869702c90317ae505ec7ba383eeae05b
suraj <surajchaunal@suraj-ka-MacBook-Pro.local>
2025-09-24 08:05:54 +0530

    added styling and markup for cart dropdown

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const menuContainer = document.querySelector(".menu--container");
 const menuOpenBtn = document.querySelector(".menu-icon--open");
 const menuCloseBtn = document.querySelector(".menu-icon--close");
 const mainNavContainer = document.querySelector(".header__nav");
+const addToCartBtn = document.querySelector(".btn-cart");
+const cartContainer = document.querySelector(".cart-container");
 
 function handleMenuclick(e) {
   if (!e.target.classList.contains("menu-icon")) return;
@@ -11,5 +13,16 @@ function handleMenuclick(e) {
   menuOpenBtn.classList.toggle("active");
   mainNavContainer.classList.toggle("active");
 }
-console.log(menuContainer);
+function handleAddToCart(e) {
+  cartContainer.classList.toggle("active");
+}
+function handleEscape(e) {
+  if (e.key != "Escape") return;
+
+  if (cartContainer.classList.contains("active"))
+    cartContainer.classList.remove("active");
+  addToCartBtn.focus();
+}
 menuContainer.addEventListener("click", handleMenuclick);
+addToCartBtn.addEventListener("click", handleAddToCart);
+window.addEventListener("keydown", handleEscape);
